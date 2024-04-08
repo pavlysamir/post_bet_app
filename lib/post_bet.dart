0a1250c75b2dc69fdb0e5cc798421ffac586c0c3
pavlysamir <bavlysamir33@gmail.com>
2024-04-08 12:29:48 +0200

    first commit

---
+++ lib/post_bet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostBetApp extends StatelessWidget {
+  const PostBetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
