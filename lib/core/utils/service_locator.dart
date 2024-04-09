66b12987e7e04de3b15c845fb5de974593071fba
pavlysamir <bavlysamir33@gmail.com>
2024-04-09 04:21:50 +0200

    Add onboarding feature with model, item, and view components
    Add the onboarding feature with model, item, and view components for displaying
    onboarding screens with images, titles, and messages. Includes logic for
    skipping and navigating to the next screen.

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,7 @@
+// import 'shared_preferences_cash_helper.dart';
+
+// final getIt = GetIt.instance;
+// void setUpServiceLocator() {
+//   getIt.registerSingleton<CashHelperSharedPreferences>(
+//       CashHelperSharedPreferences());
+// }
---
+++ lib/features/on_boarding/presentations/on_boarding_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../../../core/utils/styles.dart';
+import '../data/models/onBoeding_model.dart';
+
+class BuildBoardingItem extends StatelessWidget {
+  const BuildBoardingItem({super.key, required this.model});
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image(
+          image: AssetImage('${model.image}'),
+          height: MediaQuery.of(context).size.height * 0.4,
+          width: double.infinity,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text('${model.title}',
+            style: Styles.title1textStyle20, textAlign: TextAlign.center),
+        const SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: Text(
+            '${model.messege}',
+            style: Styles.button1textStyle16,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 5,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
