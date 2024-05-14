32b8b9c2fd473a16434e6bac27d2c2dc34e4b6e0
pavlysamir <bavlysamir33@gmail.com>
2024-05-14 06:55:23 +0300

    Refactor CreatePostPlatFormItem to StatefulWidget
    Convert CreatePostPlatFormItem from StatelessWidget to StatefulWidget for better
    state management and added functionality. This change allows for dynamic
    updates based on user interactions with checkboxes and dropdown menus.

--- lib/features/home/presentation/views/creare_post_view.dart
+++ lib/features/home/presentation/views/creare_post_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -108,7 +107,7 @@ class CreatePostView extends StatelessWidget {
   }
 }
 
-class CreatePostPlatFormItem extends StatelessWidget {
+class CreatePostPlatFormItem extends StatefulWidget {
   const CreatePostPlatFormItem({
     super.key,
     required this.paltformName,
@@ -119,19 +118,87 @@ class CreatePostPlatFormItem extends StatelessWidget {
   final String paltformIcon;
 
   @override
+  State<CreatePostPlatFormItem> createState() => _CreatePostPlatFormItemState();
+}
+
+class _CreatePostPlatFormItemState extends State<CreatePostPlatFormItem> {
+  String dropdownValue = 'Post';
+  bool initialValue = false;
+  bool initialValue2 = false;
+  final Map<String, bool> _checkBoxValues = {};
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () {
-        if (kDebugMode) {
-          print('tapped');
-        }
-      },
-      title: Text(
-        paltformName,
-        style: Theme.of(context).textTheme.titleLarge,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.paltformName,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          widget.paltformName == 'Instagram'
+              ?
+// Step 2.
+              SizedBox(
+                  width: 120.w,
+                  child: DropdownButton<String>(
+                    // Step 3. Set the initial dropdown value.
+                    value: dropdownValue,
+
+                    // Step 4. Define dropdown items with associated checkboxes.
+                    items: <String>[
+                      'Post',
+                      'Story',
+                      'Reel',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              value,
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                            StatefulBuilder(
+                              builder: (context, setState) => Checkbox(
+                                // Step 1. Use a separate state variable for each checkbox.
+                                value: _checkBoxValues[value] ??
+                                    false, // Initialize to false
+                                onChanged: (bool? newValue) {
+                                  setState(() {
+                                    _checkBoxValues[value] = newValue!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+
+                    // Step 5. Update dropdown value and handle checkbox state changes.
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                        // Reset checkboxes for other items (optional)
+                        _checkBoxValues
+                            .removeWhere((key, value) => key != newValue);
+                      });
+                    },
+                  ),
+                ) // ignore: unnecessary_this
+              : Checkbox(
+                  value: initialValue,
+                  onChanged: (value) {
+                    setState(() {
+                      initialValue = value!;
+                    });
+                  }),
+        ],
       ),
       leading: Image.asset(
-        paltformIcon,
+        widget.paltformIcon,
         height: 40.h,
         width: 40.w,
       ),
