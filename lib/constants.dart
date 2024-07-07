f020b274adb8148e3866c88bb0bfd40731fedb78
pavlysamir <bavlysamir33@gmail.com>
2024-07-07 14:35:58 +0300

    Update Android build.gradle and constants.dart, and pubspec.lock and pubspec.yaml dependencies
    - Updated compileSdkVersion and targetSdkVersion in Android build.gradle
    - Updated kPrimaryKey color in constants.dart
    - Updated intl dependency version in pubspec.yaml
    - Added multidex dependency in Android build.gradle
    - Updated leak_tracker_flutter_testing and leak_tracker_testing dependencies in pubspec.lock
    
    The commit message could be:
    
    Update Android build.gradle and constants.dart, and pubspec.lock and pubspec.yaml dependencies

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 const kProfileImage =
     'https://firebasestorage.googleapis.com/v0/b/yappy-app-ef720.appspot.com/o/posts%2FprofileImg.png?alt=media&token=1ad84aae-172b-4b68-b199-d59ab8e7107d';
-const kPrimaryKey = Color(0xffFFAE35);
+const kPrimaryKey = Color(0xff8E0000);
 const kSecondaryKey = Color(0xffD35656);
 const kBlackColor = Colors.black;
 Color kPoppingsRedColor = const Color(0xffFF0000).withOpacity(0.2);
